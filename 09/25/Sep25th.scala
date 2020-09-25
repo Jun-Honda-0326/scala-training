d68961f674744846b1288cab2334aa7c42cae328
Jun-Honda-0326 <j.honda.0326@gmail.com>
2020-09-25 18:51:20 +0900

    問題2の途中

--- 09/25/Sep25th.scala
+++ 09/25/Sep25th.scala
@@ -4,28 +4,48 @@ object Sep25th {
     val none: Option[Int] = None
     
     println(optionQuestion1(num1))
-    println(optionQuestion1(none))
-    
+    println(optionQuestion1(none)) 
+    println(optionQuestion2(num1))
   }
 
+
+//問題1 Option[Int]型である、numが引数として渡されるメソッドoptionQuestion1があります。Someの場合はその値を、Noneの場合は０を返却するメソッドを、getOrElseメソッドを使って作成してください。
 
   def optionQuestion1(num: Option[Int]):Int = {
     num.getOrElse(2)
   }
 
+
+//問題2 Option[Int]型である、numが引数として渡されるoptionQuestion2メソッドがあります。Someの場合はその値に3を足したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを、mapメソッドを使って作成してください。
+
+  def optionQuestion2(num: Option[Int]):Option[Int] = {
+    num.map(n => n * 3)
+    
+  
+
+  }
+
+
+
 }
-//1 Option[Int]型である、numが引数として渡されるメソッドoptionQuestion1があります。Someの場合はその値を、Noneの場合は０を返却するメソッドを、getOrElseメソッドを使って作成してください。
+
+
+
+
+//3 val strOpt: Option[String] = Some("") を定義し、それに対してisEmpty, isDefined を実行した場合にどのような挙動をするか確かめてください。
+
+
+
+
+//4 Option[Option[Int]]型である、numが引数として渡されるoptionQuestion4メソッドがあります。Someの場合はその値を2倍したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを作成してください。
 
   
 
     
 
-//2 Option[Int]型である、numが引数として渡されるoptionQuestion2メソッドがあります。Someの場合はその値に3を足したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを、mapメソッドを使って作成してください。
 
 
-//3 val strOpt: Option[String] = Some("") を定義し、それに対してisEmpty, isDefined を実行した場合にどのような挙動をするか確かめてください。
 
-//4 Option[Option[Int]]型である、numが引数として渡されるoptionQuestion4メソッドがあります。Someの場合はその値を2倍したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを作成してください。
 
 
 

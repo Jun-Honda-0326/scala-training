82221d26276817d93c961ee81dca8cd0214780c3
Jun-Honda-0326 <j.honda.0326@gmail.com>
2020-09-30 11:48:20 +0900

    練習問題6

--- src/main/scala/09/28/Sep28th.scala
+++ src/main/scala/09/28/Sep28th.scala
@@ -3,7 +3,8 @@ object Sep28th {
     val numSeq1: Seq[Int] = Seq(1,3,5,6,8,9,15,21)
     val numSeqSeq1: Seq[Seq[Int]] = Seq(Seq(1,3,5,6,7,9,12,24), Seq(2,4,5,7)) 
     val numOptSeq1: Seq[Option[Int]] = Seq(Some(0), Some(2), Some(3))
-
+    val strSeq1: Seq[String] = Seq("hi", "hello", "ども", "great")
+    
     println(seqQuestion1(numSeq1))
     println(seqQuestion2(numSeq1))
     println(seqQuestion3(numSeqSeq1))
@@ -11,6 +12,7 @@ object Sep28th {
     println(seqQuestion3_2(numSeqSeq1))
     println(seqQuestion4(numSeq1))
     println(seqQuestion5(numOptSeq1))
+    println(seqQuestion6(strSeq1))
   }
 
 //問題1
@@ -77,6 +79,21 @@ object Sep28th {
 //問題7
 //Seq[Int]型であるnumSeqが引数として渡されるseqQuestion7メソッドがあります。 numSeqのうち値が100以上である最初のIntを2倍した値を返すメソッドを作成してください。 100以上の値がない場合は0を返してください。 メソッドはcollectFirstを使って作成してください。
 //
+
+  //問題6
+  //Seq[String]型である、strSeqが引数として渡されるseqQuestion6メソッドがあります。 strSeqのうち文字列の長さが2以上であるものの末尾に"x”を追加し、それらのみを含むSeqを返すメソッドを、collectを使って作成してください。
+  //
+
+  def seqQuestion6(strSeq: Seq[String]): Seq[String] = {
+    strSeq.collect{case str if str.length >= 3 => str + "x"}
+
+
+  }
+
+
+
+
+
 //問題8
 //Seq[Int]であるnumSeqが引数として渡されるseqQuestion8メソッドがあります。 このうち先頭の値と末尾の値を足し合わせた値(Int)を、 Seqが空である場合は0を返すメソッドをheadOptionとlastOptionを用いて作成してください。
 //
